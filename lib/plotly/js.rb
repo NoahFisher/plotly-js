25524c0b319527a58c45db5fbcb363a6053b548c
NoahFisher <noah.fisher@gmail.com>
2016-11-29 17:57:33 +0000

    initial commit

---
+++ lib/plotly/js.rb
@@ -0,0 +1,10 @@
+require "plotly/js/version"
+
+module Plotly
+  module Js
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/plotly/js/version.rb
@@ -0,0 +1,5 @@
+module Plotly
+  module Js
+    VERSION = "1.20.5"
+  end
+end
